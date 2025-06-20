1f2c1f65edcdfee83d33ca18894c485d7df144a9
msw-Hub <swmoon7@naver.com>
2025-06-20 16:51:09 +0900

    [SEN-17] fit : 신규 사용자 대 재방문자 데이터 구조 변경

--- sena-back/src/main/java/org/example/analytics/dto/NewVsReturningDto.java
+++ sena-back/src/main/java/org/example/analytics/dto/NewVsReturningDto.java
@@ -2,8 +2,16 @@ package org.example.analytics.dto;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class NewVsReturningDto {
-    private int newUsers;
-    private int returningUsers;
+    private List<DailyUserStat> dailyStats;
+
+    @Data
+    public static class DailyUserStat {
+        private String date;         // yyyy-MM-dd
+        private int newUsers;
+        private int returningUsers;
+    }
 }

193863c349853ad6ca90db99240c14219a0b8dd4
springbbot-developer <swmoon7@naver.com>
2025-07-06 18:45:22 +0900

    [BE] 재배포

--- sena-back/src/main/java/org/example/admin/viewer/ViewerService.java
+++ sena-back/src/main/java/org/example/admin/viewer/ViewerService.java
@@ -289,6 +289,7 @@ public class ViewerService {
         response.getWriter().write("{\"message\":\"로그아웃 성공\"}");
     }
 
+    
     // 관리자 이름과 권한 조회 - 이메일로
     public Map<String, String> getAdminInfo(String email) {
         AdminEntity admin = adminRepository.findByEmail(email)
